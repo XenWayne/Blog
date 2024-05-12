75cc9f46453a24f9b5ca8bd5af66835deed09c9c
XenWayne <xenwayne@mevisual.top>
2024-05-12 12:26:44 +0800

    remoe default cover & update bangumi

---
+++ Backup/hexo-theme-butterfly/scripts/tag/link.js
@@ -0,0 +1,21 @@
+function link(args) {
+    args = args.join(' ').split(',');
+    let title = args[0];
+    let sitename = args[1];
+    let link = args[2];
+
+
+    return `<a class="tag-Link" target="_blank" href="${link}">
+    <div class="tag-link-tips">引用站外地址</div>
+    <div class="tag-link-bottom">
+        <div class="tag-link-left"><i class="fa-solid fa-link"></i></div>
+        <div class="tag-link-right">
+            <div class="tag-link-title">${title}</div>
+            <div class="tag-link-sitename">${sitename}</div>
+        </div>
+        <i class="fa-solid fa-angle-right"></i>
+    </div>
+    </a>`;
+}
+
+hexo.extend.tag.register('link',link, { ends: false })
